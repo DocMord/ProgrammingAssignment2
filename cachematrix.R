758e1641a7542187832a7415d710cb7efd6c39a2
Greg Shetler <gshetlerphd@gmail.com>
2024-08-19 21:36:44 -0600

    Update cachematrix.R
    Paste of my versions of makeCacheMatrix and cacheSolve, patterned after the example functions provided for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve are based on code provided by R. D. Peng
+## and are altered to perform the same caching behavior for a matrix and
+## its inverse
 
-## Write a short comment describing this function
+## The description for the behavior of makeCacheMatrix is based on the
+## description of the original code provided by R. D. Peng.
+## makeCacheMatrix does the following, directly reflecting the code for
+## a vector and its mean, but for a matrix and its inverse:
+## 1. set the value of the vector
+## 2. get the value of the vector
+## 3. set the value of the mean
+## 4. get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        I <- NULL
+        set <- function(y) {
+                x <<- y
+                I <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) I <<- solve
+        getinverse <- function() I
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve does the following, directly reflecting the code provided by
+## R. D. for a vector and its mean, but for a matrix and its inverse.  The
+## description of the behavior of this function is modified from the description
+## of the original code provided by R. D. Peng.
+## cacheSolve calculates the mean of the special "vector" created with makeCacheMatrix
+## However, it first checks to see if the mean has already been calculated.
+## If so, it gets the mean from the cache and skips the computation. Otherwise, it
+## calculates the mean of the data and sets the value of the mean in the cache via
+##the setmean function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        I <- x$getinverse()
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        data <- x$get()
+        I <- solve(data, ...)
+        x$setinverse(I)
+        I
 }
